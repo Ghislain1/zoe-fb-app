0429ee9b9dbf6b1b7c36d8d6ccdbb91834ef84a4
Ghislain <gzeleu@gmx.de>
2018-03-04 15:16:58 +0100

    Init the famous Hero Module

---
+++ src/app/heroes/components/hero-list.component.ts
@@ -0,0 +1,32 @@
+
+
+
+import 'rxjs/add/operator/switchMap';
+import { Observable } from 'rxjs/Observable';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+ 
+
+@Component({
+  template: `
+    <h2>HEROES</h2>
+    <ul class="items">
+      <li *ngFor="let hero of heroes$ | async"
+        [class.selected]="hero.id === selectedId">
+        <a [routerLink]="['/hero', hero.id]">
+          <span class="badge">{{ hero.id }}</span>{{ hero.name }}
+        </a>
+      </li>
+    </ul>
+
+    <button routerLink="/sidekicks">Go to sidekicks</button>
+  `
+})
+export class HeroListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
