0ad7515e66e3691d3853362674e17c7dfe0a9868
fdmt55 <git@dinolab.ges.thm.de>
2018-08-02 22:31:04 +0200

    Radikal changes

--- ghislain-one-app/src/app/app-routing.module.ts
+++ ghislain-one-app/src/app/app-routing.module.ts
@@ -1,28 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
- 
-import { AlbumDisplay} from './albums/albumDisplay';
-import {AlbumEditor } from './albums/albumEditor';
-import { ArtistList } from './artists/artistList';
-import { ArtistDisplay} from "./artists/artistDisplay";
- 
-import {LoginComponent} from "./common/login";
-import { AlbumList } from './albums/albumList';
- 
-
-const routes: Routes = [
-	{path: '', redirectTo: 'albums', pathMatch: 'full'},
-	{path: 'albums', component: AlbumList },
-	{path: 'album/:id', component: AlbumDisplay },
-	{path: 'album/edit/:id', component: AlbumEditor },
-	{path: 'artists', component: ArtistList },
-	{path: 'artist/:id', component: ArtistDisplay },
-//	{path: 'options', component: OptionsComponent },
-	{ path: 'login', component: LoginComponent },
-	{ path: 'logout', component: LoginComponent },
-//	{ path: 'about', component: AboutComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ghislain-one-app/src/app/app.module.ts
+++ ghislain-one-app/src/app/app.module.ts
@@ -1,98 +1,36 @@
-import {NgModule, Injectable} from '@angular/core'
-import {AppRoutingModule} from "./app-routing.module";
-import {AppComponent} from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
-import {FormsModule} from "@angular/forms";
-import {BrowserModule} from "@angular/platform-browser";
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { HttpModule } from "@angular/http";  // legacy
-import {HttpClientModule} from "@angular/common/http";   // use this
 
-// components
- 
-import {AppFooter} from "./common/appFooter";
 
- 
+// import {HttpClient} from "./business/HttpClient";
+// import {AboutComponent} from "./options/about";
 
-import {AlbumDisplay} from './albums/albumDisplay';
-import {AlbumEditor} from './albums/albumEditor';
-import {ArtistList} from './artists/artistList';
-import {ArtistDisplay} from './artists/artistDisplay';
-
-// services
- 
-import {AlbumService} from './albums/albumService';
-import {ArtistService} from './artists/artistService';
-import {AppConfiguration} from './business/appConfiguration';
-import {UserInfo} from "./business/userInfo";
-
-import {LocationStrategy, HashLocationStrategy} from '@angular/common';
-import {AlbumSongList} from "./albums/albumSongList";
-
-// directives and shared components
-import {ErrorDisplay, ErrorInfo} from './common/errorDisplay';
-import {ArtistEditor} from "./artists/artistEditor";
-//import {OptionsComponent} from "./Options/options";
-import {LoginComponent} from "./common/login";
-import { Album } from './business/album';
-import { Artist } from './business/artist';
-import { Track } from './business/Track';
-import { AlbumList } from './albums/albumList';
-import { AppHeader } from './common/appHeader';
-
-//import {HttpClient} from "./business/HttpClient";
-//import {AboutComponent} from "./options/about";
-
-//declare var $:any;
-//declare var toastr:any;
+// declare var $:any;
+// declare var toastr:any;
 // Enable production mode
 // import { enableProdMode } from '@angular/core';
 // enableProdMode();
 @NgModule({
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        FormsModule,
-        HttpModule,
-        HttpClientModule,
-        AppRoutingModule
-    ],
+  imports: [
 
-    // components
-    declarations: [
-        AppComponent,
-        AppHeader,
-        AppFooter,
+    AppRoutingModule
+  ],
 
-      //AboutComponent,
-        AlbumList,
-        AlbumDisplay,
-        AlbumEditor,
-        AlbumSongList,
-        ArtistList,
-        ArtistDisplay,
-        ArtistEditor,
-        ErrorDisplay,
+  // components
+  declarations: [
+    AppComponent,
 
-        LoginComponent,
-      //  OptionsComponent
 
-    ],
-    // services, pipes and providers
-    providers: [
-        AlbumService,
-        ArtistService,
-        AppConfiguration,
-        UserInfo,
-        ErrorInfo,
-        Album,
-         Artist,
-          Track
+  ],
+  // services, pipes and providers
+  providers: [
 
-      
-    ],
 
-    bootstrap: [AppComponent]
+
+  ],
+
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 
