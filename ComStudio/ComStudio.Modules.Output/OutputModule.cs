9cfccb74821a4e3ba40bae9eead954eafe49edee
GHislain1 <Gzeleu@gmx.de>
2018-10-17 16:46:28 +0200

    Fix Module with comstduo Prism

--- ComStudio/ComStudio.Modules.Output/OutputModule.cs
+++ ComStudio/ComStudio.Modules.Output/OutputModule.cs
@@ -1,22 +1,48 @@
 ﻿namespace ComStudio.Modules.Output
 {
-    using Microsoft.Practices.Unity;
-    using Prism.Modularity;
-    using Prism.Regions;
+  using Microsoft.Practices.Unity;
+  using Prism.Modularity;
+  using Prism.Regions;
+  using System.Windows;
+  using System.Windows.Controls;
+  using System.Windows.Data;
+  using System.Windows.Media;
 
-    public class OutputModule : IModule
+  public class OutputModule : IModule
+  {
+    private readonly IUnityContainer container;
+    private readonly IRegionManager regionManager;
+
+    public OutputModule(IUnityContainer container, IRegionManager regionManager)
     {
-        private readonly IUnityContainer container;
-        private readonly IRegionManager regionManager;
-
-        public OutputModule(IUnityContainer container, IRegionManager regionManager)
-        {
-            this.container = container;
-            this.regionManager = regionManager;
-        }
-
-        public void Initialize()
-        {
-        }
+      this.container = container;
+      this.regionManager = regionManager;
     }
+
+    public void Initialize()
+    {
+      var image = this.CreateImage();
+    }
+
+    /// <summary>
+    /// Creates the image.
+    /// </summary>
+    /// <returns>Image.</returns>
+    private Image CreateImage()
+    {
+      var img = new Image();
+
+      BindingOperations.SetBinding(img, RenderOptions.BitmapScalingModeProperty, new Binding
+      {
+        Path = new PropertyPath(RenderOptions.BitmapScalingModeProperty),
+        Source = this,
+      });
+
+      img.Stretch = Stretch.None;
+      img.HorizontalAlignment = HorizontalAlignment.Left;
+      img.VerticalAlignment = VerticalAlignment.Top;
+
+      return img;
+    }
+  }
 }
--- ComStudio/ComStudio.Modules.Watch/WatchModule.cs
+++ ComStudio/ComStudio.Modules.Watch/WatchModule.cs
@@ -1,33 +1,33 @@
 ﻿namespace ComStudio.Modules.Watch
 {
-    using ComStudio.Infrastructure;
-    using ComStudio.Modules.Watch.Services;
-    using ComStudio.Modules.Watch.ViewModels;
-    using ComStudio.Modules.Watch.Views;
-    using Microsoft.Practices.Unity;
-    using Prism.Modularity;
-    using Prism.Regions;
+  using ComStudio.Infrastructure;
+  using ComStudio.Modules.Watch.Services;
+  using ComStudio.Modules.Watch.ViewModels;
+  using ComStudio.Modules.Watch.Views;
+  using Microsoft.Practices.Unity;
+  using Prism.Modularity;
+  using Prism.Regions;
 
-    public class WatchModule : IModule
+  public class WatchModule : IModule
+  {
+    private readonly IUnityContainer container;
+    private readonly IRegionManager regionManager;
+
+    public WatchModule(IUnityContainer container, IRegionManager regionManager)
     {
-        private readonly IUnityContainer container;
-        private readonly IRegionManager regionManager;
-
-        public WatchModule(IUnityContainer container, IRegionManager regionManager)
-        {
-            this.container = container;
-            this.regionManager = regionManager;
-        }
-
-        public void Initialize()
-        {
-            this.container.RegisterType<IWatchListService, WatchListService>();
-            this.container.RegisterType<AddWatchViewModel, AddWatchViewModel>();
-            this.container.RegisterType<WatchListViewModel, WatchListViewModel>();
-            this.regionManager.RegisterViewWithRegion(RegionNames.MainToolBarRegion,
-                                                       () => this.container.Resolve<AddWatchView>());
-            this.regionManager.RegisterViewWithRegion(RegionNames.MainRegion,
-                                                       () => this.container.Resolve<WatchListView>());
-        }
+      this.container = container;
+      this.regionManager = regionManager;
     }
+
+    public void Initialize()
+    {
+      this.container.RegisterType<IWatchListService, WatchListService>();
+      this.container.RegisterType<AddWatchViewModel, AddWatchViewModel>();
+      this.container.RegisterType<WatchListViewModel, WatchListViewModel>();
+      this.regionManager.RegisterViewWithRegion(RegionNames.MainToolBarRegion,
+                                                 () => this.container.Resolve<AddWatchView>());
+      //this.regionManager.RegisterViewWithRegion(RegionNames.MainRegion,
+      //                                           () => this.container.Resolve<WatchListView>());
+    }
+  }
 }
