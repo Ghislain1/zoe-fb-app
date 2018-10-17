763da3d4de7bb2165d74d8fa2b1dcf002496e614
GHislain1 <Gzeleu@gmx.de>
2018-10-17 16:11:31 +0200

    ComStudio Server uses App.config

--- ComStudio/ComStudio/ComStudioBootstrapper.cs
+++ ComStudio/ComStudio/ComStudioBootstrapper.cs
@@ -1,65 +1,53 @@
 ﻿namespace ComStudio
 {
-    using ComStudio.ViewModels;
-    using ComStudio.Views;
-    using Microsoft.Practices.ServiceLocation;
-    using Microsoft.Practices.Unity;
-    using Prism.Modularity;
-    using Prism.Regions;
-    using Prism.Unity;
+  using ComStudio.ViewModels;
+  using ComStudio.Views;
+  using Microsoft.Practices.ServiceLocation;
+  using Microsoft.Practices.Unity;
+  using Prism.Modularity;
+  using Prism.Regions;
+  using Prism.Unity;
 
-    using System.Windows;
+  using System.Windows;
 
-    public class ComStudioBootstrapper : UnityBootstrapper
+  public class ComStudioBootstrapper : UnityBootstrapper
+  {
+    protected override Prism.Regions.IRegionBehaviorFactory ConfigureDefaultRegionBehaviors()
     {
-        protected override Prism.Regions.IRegionBehaviorFactory ConfigureDefaultRegionBehaviors()
-        {
-            var factory = base.ConfigureDefaultRegionBehaviors();
-            return factory;
-        }
-
-        protected override void ConfigureModuleCatalog()
-        {
-            base.ConfigureModuleCatalog();
-
-            //TODO: GHislain Why Reihenfolge spielt eine große role--->
-            ModuleCatalog moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
-            moduleCatalog.AddModule(typeof(ComStudio.Modules.Output.OutputModule));
-            moduleCatalog.AddModule(typeof(ComStudio.Modules.Startpage.StartpageModule));
-
-            moduleCatalog.AddModule(typeof(ComStudio.Modules.Market.MarketModule));
-            moduleCatalog.AddModule(typeof(ComStudio.Modules.Position.PositionModule));
-
-            moduleCatalog.AddModule(typeof(ComStudio.Modules.Watch.WatchModule));
-
-            moduleCatalog.AddModule(typeof(ComStudio.Modules.Topology.TopologyModule));
-        }
-
-        /// <summary>
-        /// Configures the LocatorProvider for the <see cref="Microsoft.Practices.ServiceLocation.ServiceLocator"/>.
-        /// </summary>
-        protected override void ConfigureServiceLocator()
-        {
-            // ServiceLocator.SetLocatorProvider();
-            UnityServiceLocator locator = new UnityServiceLocator(this.Container);
-            ServiceLocator.SetLocatorProvider(() => locator);
-            var ff = ServiceLocator.Current;
-            IRegionManager regionManager = ServiceLocator.Current.GetInstance<IRegionManager>();
-        }
-
-        protected override DependencyObject CreateShell()
-        {
-            // Use the container to create an instance of the shell.
-            Shell view = this.Container.TryResolve<Shell>();
-            view.DataContext = new ShellViewModel();
-            return view;
-        }
-
-        protected override void InitializeShell()
-        {
-            base.InitializeShell();
-            App.Current.MainWindow = (Window)this.Shell;
-            App.Current.MainWindow.Show();
-        }
+      var factory = base.ConfigureDefaultRegionBehaviors();
+      return factory;
     }
+
+    /// <summary>
+    /// Configures the LocatorProvider for the <see cref="Microsoft.Practices.ServiceLocation.ServiceLocator"/>.
+    /// </summary>
+    protected override void ConfigureServiceLocator()
+    {
+      // ServiceLocator.SetLocatorProvider();
+      UnityServiceLocator locator = new UnityServiceLocator(this.Container);
+      ServiceLocator.SetLocatorProvider(() => locator);
+      var ff = ServiceLocator.Current;
+      IRegionManager regionManager = ServiceLocator.Current.GetInstance<IRegionManager>();
+    }
+
+    protected override IModuleCatalog CreateModuleCatalog()
+    {
+      return new ConfigurationModuleCatalog();
+    }
+
+    protected override DependencyObject CreateShell()
+    {
+      // Use the container to create an instance of the shell.
+      Shell view = this.Container.TryResolve<Shell>();
+      view.DataContext = new ShellViewModel();
+      return view;
+    }
+
+    protected override void InitializeShell()
+    {
+      base.InitializeShell();
+      App.Current.MainWindow = (Window)this.Shell;
+      App.Current.MainWindow.Show();
+    }
+  }
 }
