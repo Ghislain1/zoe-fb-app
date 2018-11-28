4820669875503b536f9e87a56e8f4856414dcef8
fdmt55 <git@dinolab.ges.thm.de>
2018-11-29 00:09:03 +0100

    Comish

---
+++ ComStudio/ComStudio.Modules.WebTopology/WebTopologyModule.cs
@@ -0,0 +1,33 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComStudio.Modules.WebTopology
+{
+    public class WebTopologyModule : IModule
+    {
+        public WebTopologyModule()
+        {
+        }
+
+        public void Initialize(IContainerProvider containerProvider)
+        {
+        }
+
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            //TOD refatory programm class !!
+            Program.Main(null);
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //containerRegistry.RegisterSingleton<ITopologyService, TopologyService>();
+
+            //containerRegistry.RegisterSingleton<TopologyViewModel, TopologyViewModel>();
+        }
+    }
+}
--- ComStudio/ComStudio/App.xaml.cs
+++ ComStudio/ComStudio/App.xaml.cs
@@ -5,6 +5,9 @@
     using Prism.Ioc;
     using Prism.Modularity;
     using Prism.Unity;
+    using System;
+    using System.IO;
+    using System.Reflection;
     using System.Windows;
 
     /// <summary>
@@ -17,6 +20,9 @@
         {
             ICefSharpProvider cefSharpProvider = new CefSharpProvider();
             cefSharpProvider.InitializeCefSharp();
+
+            //Add Custom assembly resolver
+            AppDomain.CurrentDomain.AssemblyResolve += Resolver;
         }
 
         protected override IModuleCatalog CreateModuleCatalog()
@@ -31,6 +37,23 @@
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
+        }
+
+        private Assembly Resolver(object sender, ResolveEventArgs args)
+        {
+            Assembly toReturnAssembly = null;
+            if (args.Name.Contains("WebTopology"))
+            {
+                string assemblyName = args.Name.Split(new[] { ',' }, 2)[0] + ".dll";
+                string archSpecificPath = Path.Combine(assemblyName);
+
+                if (File.Exists(args.Name))
+                {
+                    toReturnAssembly = Assembly.LoadFile(archSpecificPath);
+                }
+            }
+
+            return toReturnAssembly;
         }
     }
 }
