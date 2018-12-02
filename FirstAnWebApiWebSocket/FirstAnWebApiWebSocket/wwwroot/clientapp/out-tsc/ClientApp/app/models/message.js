6873809f688788ca9bf4d1d8b8d890e61ce339e4
fdmt55 <git@dinolab.ges.thm.de>
2018-12-02 10:59:39 +0100

    Refactory

--- FirstAnWebApiWebSocket/FirstAnWebApiWebSocket/wwwroot/clientapp/out-tsc/ClientApp/app/models/message.js
+++
@@ -1,5 +0,0 @@
-// export interface Message {
-//     author: string;
-//     message: string;
-// }
-//# sourceMappingURL=message.js.map
--- FirstAnWebApiWebSocket/FirstAnWebApiWebSocket/wwwroot/clientapp/out-tsc/wwwroot/clientapp/out-tsc/app/models/message.js
+++
@@ -1,6 +0,0 @@
-// export interface Message {
-//     author: string;
-//     message: string;
-// }
-//# sourceMappingURL=message.js.map
-//# sourceMappingURL=message.js.map
