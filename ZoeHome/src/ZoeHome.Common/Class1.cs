98410a85862eb50f1bd110e3eb96cb9b214c91b3
fdmt55 <git@dinolab.ges.thm.de>
2018-12-15 12:47:36 +0100

    RE- strutrure with bes architecture

---
+++ ZoeHome/src/ZoeHome.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ZoeHome.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ ZoeHome/src/ZoeHome.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ZoeHome.Domain
+{
+    public class Class1
+    {
+    }
+}
